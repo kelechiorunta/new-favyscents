a3365001ec23e55154bc8df3eb967dcee4b0415b
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-02-20 17:11:03 +0100

    Implemented the cart page components

--- src/components/shoppingcart/OrderSection.jsx
+++ src/components/shoppingcart/OrderSection.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './OrderSection.css'
+import { FaShoppingBag } from 'react-icons/fa';
 
 export default function OrderSection() {
   return (
     <div className='order-section'>
-      OrderSection
+      <div className="subtotal-section">
+          <div className="subtotal-container">
+            <p className="subtotal">Subtotal</p>
+            <p className="result">$195</p>
+          </div>
+          <div className="tax-container">
+            <p className="tax">Tax</p>
+            <p className="result">$0</p>
+          </div>    
+          <div className="total-container">
+            <p className="total">TOTAL</p>
+            <p className="result">$195</p>
+          </div>      
+      </div>
+      <hr style={{width: '100%', height: '2px', margin: '24'}}/>
+    
+      <button className="checkout">
+          <FaShoppingBag className='checkout-icon' />
+          PROCEED TO SECURE CHECKOUT
+      </button>
     </div>
   )
 }
