b14b5765ef7636e1e5131ab542edf512036d9cef
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-03-15 10:46:31 +0100

    Implemented a logic extraction for ShippingForm and created env for allowed cors domains

--- src/components/checkout/NewForm.jsx
+++ src/components/checkout/NewForm.jsx
@@ -8,6 +8,7 @@ import OrderSection from '../shoppingcart/OrderSection.jsx';
 import { ViewChild, ViewProvider } from '../ViewContext/ViewContext.jsx';
 import Divider from '../divider/Divider.jsx';
 import DividerII from '../dividerII/DividerII.jsx';
+import ShippingForm from './ShippingForm.jsx';
 
 export default function NewForm({action, pending}) {
     const formRef = useRef(null);
@@ -39,71 +40,19 @@ export default function NewForm({action, pending}) {
             <section style={{display: 'flex', gap: '2rem', alignItems: 'center'}}>
                 <div className="shipping-section">
                     <h1 style={{marginBottom: '2rem', marginTop: '4rem'}}>SHIPPING ADDRESS</h1>
-                    <form ref={formRef} onSubmit={handleAction} style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
-                        <div style={{display: 'flex', gap: '1rem', width:'600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='message'>Message</label>
-                            <input style={{width: 400}} type="text" name='message' />
-                        </div>
-                        <div style={{display: 'flex', gap: '1rem', width: '600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='email'>Email Address</label>
-                            <input style={{width: 400}} type="text" name='email' />
-                        </div>
-                        <div style={{display: 'flex', gap: '1rem', width: '600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='firstname'>First Name</label>
-                            <input style={{width: 400}} type="text" name='firstname' />
-                        </div>
-                        <div style={{display: 'flex', gap: '1rem', width: '600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='zip'>Zip Code</label>
-                            <input style={{width: 400}} type="text" name='zip' />
-                        </div>
-                        <p>You Already Have An Account With Us? <span><Link to={'/account'}>Sign In</Link> or Continue As Guest</span></p>
-                        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                            <button  type='submit' style={{position: 'relative', padding: '0.7rem', width: '120px', cursor: 'pointer'}}>
-                                LOGIN
-                                {pending && <Loader/>}
-                            </button>
-                            <Link to='/'>Forgot Password?</Link>
-                        </div>
-                        
-                    </form>
+                    
                     {/* Top form  */}
+                    <ShippingForm ref={formRef} handleAction={handleAction} pending={pending}/>
                     <hr style={{margin: '50px auto', width:'100%', height:5}}/>
 
-                    <form ref={formRef} onSubmit={handleAction} style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
-                        <div style={{display: 'flex', gap: '1rem', width:'600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='message'>Message</label>
-                            <input style={{width: 400}} type="text" name='message' />
-                        </div>
-                        <div style={{display: 'flex', gap: '1rem', width: '600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='email'>Email Address</label>
-                            <input style={{width: 400}} type="text" name='email' />
-                        </div>
-                        <div style={{display: 'flex', gap: '1rem', width: '600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='firstname'>First Name</label>
-                            <input style={{width: 400}} type="text" name='firstname' />
-                        </div>
-                        <div style={{display: 'flex', gap: '1rem', width: '600px', alignItems: 'center', justifyContent: 'space-between'}}>
-                            <label htmlFor='zip'>Zip Code</label>
-                            <input style={{width: 400}} type="text" name='zip' />
-                        </div>
-                        <p>You Already Have An Account With Us? <span><Link to={'/account'}>Sign In</Link> or Continue As Guest</span></p>
-                        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                            <button  type='submit' style={{position: 'relative', padding: '0.7rem', width: '120px', cursor: 'pointer'}}>
-                                LOGIN
-                                {pending && <Loader/>}
-                            </button>
-                            <Link to='/'>Forgot Password?</Link>
-                        </div>
-                        
-                    </form>
+                    <ShippingForm ref={formRef} handleAction={handleAction} pending={pending}/>
+                    
                 </div>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
                     <OrderSection />
                     <OrderSection />
                     <OrderSection />
                 </div>
-               
-
             </section>
         </ViewChild>
     </div>
